06b43a396420d60c53701dedf2b57f899fe8d274
Tomas Hluchan <tomas.hluchan@gmail.com>
2013-02-08 14:17:12 +0100

    render

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import webapp2
-import jinja2
 import os
 
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+from webapp2_extras import jinja2
 
-class MainPage(webapp2.RequestHandler):
+
+
+class BaseHandler(webapp2.RequestHandler):
+    @webapp2.cached_property
+    def jinja2(self):
+        return jinja2.get_jinja2(app=self.app)
+
+    def render_response(self, _template, **context):
+        rv = self.jinja2.render_template(_template, **context)
+        self.response.write(rv)
+
+
+
+class MainPage(BaseHandler):
     def get(self):
-        template_values = {
-            'name': 'webapp'
-        }
-        template = jinja_environment.get_template('index.html')
-        self.response.out.write(template.render(template_values))
-
+        context = {'name': 'webapp'}
+        self.render_response('index.html',**context)
 
 app = webapp2.WSGIApplication([
     (r'/', MainPage),
